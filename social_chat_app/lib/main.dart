72d07a9b67be5842c9dc4591b019262de5d27db2
Lithara Perera <litharaperera2002@gmail.com>
2023-09-04 11:44:01 +0530

    modified

--- social_chat_app/lib/main.dart
+++ social_chat_app/lib/main.dart
@@ -8,11 +8,35 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Column(
+      home: Scaffold(
+        backgroundColor: Colors.blue[900],
+        body: Column(
           children: [
             Text('Hello, Welcome back!'),
             Text('Login to continue'),
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Username',
+              ),
+            ),
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Password',
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                print('Forgot is clicked');
+              },
+              child: Text('Forgot Password?'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                print('Login is clicked');
+              },
+              child: Text('Login'),
+            ),
+            Text('or Sign in with'),
           ],
         ),
       ),
