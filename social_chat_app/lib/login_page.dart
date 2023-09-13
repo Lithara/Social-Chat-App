5d727d24915a7f6a94b97a015e7fb01e971b0249
Lithara Perera <litharaperera2002@gmail.com>
2023-09-13 16:29:02 +0530

    modified

--- social_chat_app/lib/login_page.dart
+++ social_chat_app/lib/login_page.dart
@@ -9,7 +9,7 @@ class LoginPage extends StatelessWidget {
       backgroundColor: Colors.blue[900],
       body: Column(
         children: [
-          Text(
+          const Text(
             'Hello, Welcome back!',
             style: TextStyle(
               color: Colors.white,
@@ -17,50 +17,101 @@ class LoginPage extends StatelessWidget {
               fontWeight: FontWeight.bold,
             ),
           ),
-          SizedBox(
+          const SizedBox(
             height: 16,
           ),
-          Text(
+          const Text(
             'Login to continue',
             style: TextStyle(
               color: Colors.white,
             ),
           ),
+          const SizedBox(
+            height: 16,
+          ),
           TextField(
             decoration: InputDecoration(
               hintText: 'Username',
+              border: const OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(12)),
+              ),
+              filled: true,
+              fillColor: Colors.white.withOpacity(0.5),
             ),
+          ),
+          const SizedBox(
+            height: 16,
           ),
           TextField(
             decoration: InputDecoration(
               hintText: 'Password',
+              border: const OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(12)),
+              ),
+              filled: true,
+              fillColor: Colors.white.withOpacity(0.5),
             ),
           ),
-          TextButton(
-            onPressed: () {
-              print('Forgot is clicked');
-            },
-            child: Text('Forgot Password?'),
+          Align(
+            alignment: Alignment.centerRight,
+            child: TextButton(
+              onPressed: () {
+                print('Forgot is clicked');
+              },
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.white,
+              ),
+              child: const Text('Forgot Password?'),
+            ),
           ),
-          ElevatedButton(
-            onPressed: () {
-              print('Login is clicked');
-            },
-            child: Text('Login'),
+          SizedBox(
+            width: 250,
+            child: ElevatedButton(
+              onPressed: () {
+                print('Login is clicked');
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.amber,
+                foregroundColor: Colors.black
+              ),
+              child: const Text('Login'),
+            ),
           ),
-          Text('or Sign in with'),
+          const SizedBox(
+            height: 62,
+          ),
+          const Text(
+            'or Sign in with',
+             style: TextStyle(
+               color: Colors.white,
+             ),
+          ),
+          const SizedBox(
+            height: 16,
+          ),
           ElevatedButton(
             onPressed: () {
               print('Google is clicked');
             },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.white,
+              foregroundColor: Colors.black,
+              shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(50)),
+              ),
+            ),
             child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Image.asset(
                   'assets/images/google.png',
                   width: 20,
                   height: 20,
                 ),
-                Text('Login with Google'),
+                const SizedBox(
+                  width: 12,
+                ),
+                const Text('Login with Google'),
               ],
             ),
           ),
@@ -68,25 +119,49 @@ class LoginPage extends StatelessWidget {
             onPressed: () {
               print('Facebook is clicked');
             },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.white,
+              foregroundColor: Colors.black,
+              shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(50)),
+              ),
+            ),
             child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Image.asset(
                   'assets/images/facebook.png',
                   width: 20,
                   height: 20,
                 ),
-                Text('Login with Facebook'),
+                const SizedBox(
+                  width: 12,
+                ),
+                const Text('Login with Facebook'),
               ],
             ),
           ),
           Row(
             children: [
-              Text("Don't have an account? "),
+              const Text(
+                "Don't have an account? ",
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
               TextButton(
                 onPressed: () {
                   print('Sign up is clicked');
                 },
-                child: Text('Sign up'),
+                style: TextButton.styleFrom(
+                  foregroundColor: Colors.amber,
+                ),
+                child: const Text(
+                  'Sign up',
+                  style: TextStyle(
+                    decoration: TextDecoration.underline,
+                  ),
+                ),
               ),
             ],
           )
