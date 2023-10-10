d744fe21c5130b361a6cca1c03efa6ac06d5bb66
Lithara Perera <litharaperera2002@gmail.com>
2023-10-10 21:13:30 +0530

    modified

---
+++ social_chat_app/lib/config/app_routes.dart
@@ -0,0 +1,18 @@
+import 'package:social_chat_app/pages/edit_profile_page.dart';
+import 'package:social_chat_app/pages/home_page.dart';
+import 'package:social_chat_app/pages/main_page.dart';
+
+class AppRoutes {
+  
+  static final pages = {
+    // '/': (context) => LoginPage(),
+    '/home': (context) => HomePage(),
+    '/main': (context) => MainPage(),
+    '/edit_profile': (context) => EditProfilePage(),
+  };
+
+  static const login = '/';
+  static const home = '/home';
+  static const main = '/main';
+  static const editProfile = '/edit_profile';
+}
---
+++ social_chat_app/lib/config/app_strings.dart
@@ -0,0 +1,27 @@
+class AppStrings{
+  static const appName = '5minuteFlutter';
+  static const helloWelcome = 'Hello, welcome back!';
+  static const loginToContinue = 'Login to continue';
+  static const username = 'Username';
+  static const password = 'Password';
+  static const forgotPassword = 'Forgot Password?';
+  static const orSignInWith = 'Or sign in with';
+  static const loginWithGoogle = 'Login with Google';
+  static const loginWithFacebook = 'Login with Facebook';
+  static const dontHaveAccount = 'Don\'t have an account?';
+  static const signUp = 'Sign up';
+  static const home = 'Home';
+  static const favorites = 'Favorites';
+  static const add = 'Add';
+  static const messages = 'Messages';
+  static const user = 'User';
+  static const followers = 'Followers';
+  static const posts = 'Posts';
+  static const following = 'Following';
+  static const profile = 'Profile';
+  static const editProfile = 'Edit Profile';
+  static const firstName = 'First Name';
+  static const lastName = 'Last Name';
+  static const phoneNumber = 'Phone Number';
+  static const location = 'Location';
+}
